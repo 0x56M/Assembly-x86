2afabae4c78ec169574a4fa601d21e174a38701c
0x56M <149252600+0x56M@users.noreply.github.com>
2026-06-11 19:31:47 -0300

    Update HelloWorld.asm
    Removed macro definitions for sys_write and sys_exit from HelloWorld.asm.

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -14,20 +14,3 @@ _start:
 	sys_write
 	%include "syscallexit.asm"
 	sys_exit
-
-;Criamos os dois arquivos abaixo que contém o conteúdo das macros que serão lidas pelas diretivas "%include" e chamadas pelo nome digitado, conforme o arquivo acima
-;Cada macro ficará em um arquivo diferente
-
-	%macro sys_write 0
-		mov eax, 1
-		mov edi, 1
-		mov esi, msg
-		mov edx, MSG_SIZE
-		syscall
-	%endmacro
-
-	%macro sys_exit 0
-		mov eax, 60
-		xor edi, edi
-		syscall
-	%endmacro
